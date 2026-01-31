01784e5f2453e4ee9de6e64cfbf72db994fd2055
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-31 18:55:35 -0300

    Aula: Presenter de perguntas

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
 import z from 'zod'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { QuestionPresenter } from '../presenters/question-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -21,8 +22,12 @@ export class FetchRecentQuestionsController {
 
   @Get()
   async handle(@Query('page', validator) page: PageQueryParam) {
-    const questions = await this.fetchRecentQuestions.execute({ page })
+    const result = await this.fetchRecentQuestions.execute({ page })
 
-    return { questions }
+    if (result.isLeft()) {
+      throw new Error()
+    }
+    const { questions } = result.value
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
   }
 }
